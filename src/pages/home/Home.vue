fb099e656f624b57f5974db005d9af8a557d9c62
JosephHo <32008dianchenan@163.com>
2018-09-05 12:06:16 +0800

    add header

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<home-header></home-header>
+	</div>
+</template>
+
+<script>
+//引入局部组件
+import HomeHeader from './components/header'
+export default {
+	name: 'Home',
+	components: {
+		HomeHeader
+	}
+}
+</script>
+
+<style scoped>
+	
+</style>
